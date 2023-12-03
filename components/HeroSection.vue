ca624621c80410f799378f881399947d1f0ef748
Huy Van Pham <87406306+huypv2002@users.noreply.github.com>
2023-12-03 22:50:39 +0700

    Update

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,13 +1,123 @@
 <template>
-$END$
+  <b-container fluid class="hero-section">
+    <b-row class="hero-section-col" cols="1" cols-sm="2" cols-md="4" cols-lg="6">
+      <b-col class="text-hero">TRANG CHỦ</b-col>
+      <b-col class="text-hero-support">|</b-col>
+      <b-col class="text-hero">GIÁO TRÌNH ĐẠI CƯƠNG</b-col>
+      <b-col class="text-hero-support">|</b-col>
+      <b-col class="text-hero">GIÁO TRÌNH THEO KHOA</b-col>
+      <b-col class="text-hero-support">|</b-col>
+      <b-col class="text-hero">GIÁO TRÌNH ĐỀ THI</b-col>
+      <b-col class="text-hero-support">|</b-col>
+      <b-col class="text-hero">LIÊN HỆ</b-col>
+    </b-row>
+    <b-row>
+      <b-col md="6" class="text-section">
+        <h1>HUITZone EDU</h1>
+        <p class="subtitle">Góc giáo trình HUIT</p>
+        <p>Trọn bộ giáo trình Tân sinh viên Đại học không khó cùng HUITZone</p>
+        <b-button variant="primary" class="mt-3">Tìm hiểu thêm</b-button>
+        <b-carousel id="carousel-1" :interval="4000" controls indicators>
+          <!-- Slides with image content -->
+          <b-carousel-slide caption="Slide 1" img-src="path-to-your-image-1.jpg"></b-carousel-slide>
+          <!-- Add more slides as needed -->
+        </b-carousel>
+      </b-col>
+      <b-col md="6" class="image-section">
+        <!-- Image goes here, adjust path accordingly -->
+        <b-img src="path-to-your-building-image.jpg" alt="HUIT Building"></b-img>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 export default {
-name: "HeroSection"
-}
+  name: 'HeroSection',
+  // You can add your script here if needed
+};
 </script>
 
 <style scoped>
+.hero-section {
+  padding: 40px 0;
+  justify-content: center;
+  background-color: #f8f9fa; /* Or any other background color you prefer */
+}
+.text-hero {
+  color: #111;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: 2.16px;
+  text-transform: capitalize;
+}
+.text-section h1 {
+  color: #004085; /* Or any other color you prefer */
+  margin-bottom: 0.5rem;
+}
+.hero-section-col {
+  justify-content: center;
+}
+.text-hero-support {
+  color: #D1D1D1;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: 2.16px;
+  text-transform: capitalize;
+}
+.text-section .subtitle {
+  font-size: 1.25rem;
+  color: #007bff; /* Or any other color you prefer */
+}
 
+.text-section p {
+  font-size: 1rem;
+  margin-bottom: 1rem;
+}
+
+.image-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+/* Customize Bootstrap button styles if needed */
+.btn-primary {
+  background-color: #007bff; /* Or use your brand primary color */
+  border-color: #007bff; /* Or use your brand primary color */
+}
+
+/* Customize the carousel indicators */
+.carousel-indicators li {
+  background-color: #007bff; /* Or use your brand primary color */
+}
+
+/* Ensure your images in the carousel are displayed correctly */
+.b-carousel-slide {
+  text-align: center;
+}
+.b-carousel-slide img {
+  width: auto;
+  max-height: 500px; /* Adjust this value based on your design */
+  margin: 0 auto;
+}
+
+/* Additional responsive styles if needed */
+@media (max-width: 768px) {
+  .hero-section {
+    padding: 20px 0;
+  }
+
+  .text-section, .image-section {
+    text-align: center;
+  }
+
+  .image-section {
+    margin-top: 20px;
+  }
+}
 </style>
--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,154 +1,65 @@
 <template>
-  <div>
-    <Header></Header>
-    <!-- Secondary Header for Sorting -->
-    <b-navbar type="light" variant="light">
-      <b-form-select v-model="sortKey" @change="sortProducts" class="ml-auto" :options="sortOptions"></b-form-select>
-    </b-navbar>
+  <b-container fluid>
+    <!-- Header Section -->
+    <b-row class="mb-3">
+      <b-col>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+          <!-- Content for navbar -->
+        </b-navbar>
+      </b-col>
+    </b-row>
 
-    <!-- Main Content Area -->
-    <b-container fluid>
-      <b-row>
+    <!-- Main Content Section -->
+    <b-row>
+      <b-col cols="12">
+        <!-- Cards with images and text -->
+        <b-card-group deck>
+          <b-card
+            v-for="card in cards"
+            :key="card.title"
+            :title="card.title"
+            :img-src="card.img"
+            img-alt="Card image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              {{ card.text }}
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+      </b-col>
+    </b-row>
 
-        <!-- Sidebar with Filters -->
-        <b-col cols="3">
-          <!-- Filters -->
-          <b-form-group label="Filter by Brand:">
-            <b-form-radio-group v-model="filters.brand">
-              <!-- Generate radio buttons from an array of brands -->
-              <b-form-radio v-for="brand in brands" :key="brand" :value="brand">{{ brand }}</b-form-radio>
-            </b-form-radio-group>
-          </b-form-group>
-          <b-form-group label="Filter by Brand:">
-            <b-form-radio-group v-model="filters.brand">
-              <!-- Generate radio buttons from an array of brands -->
-              <b-form-radio v-for="brand in brands" :key="brand" :value="brand">{{ brand }}</b-form-radio>
-            </b-form-radio-group>
-          </b-form-group>
-          <b-form-group label="Filter by Brand:">
-            <b-form-radio-group v-model="filters.brand">
-              <!-- Generate radio buttons from an array of brands -->
-              <b-form-radio v-for="brand in brands" :key="brand" :value="brand">{{ brand }}</b-form-radio>
-            </b-form-radio-group>
-          </b-form-group>
-          <b-form-group label="Filter by Brand:">
-            <b-form-radio-group v-model="filters.brand">
-              <!-- Generate radio buttons from an array of brands -->
-              <b-form-radio v-for="brand in brands" :key="brand" :value="brand">{{ brand }}</b-form-radio>
-            </b-form-radio-group>
-          </b-form-group>
-          <b-form-group label="Filter by Brand:">
-            <b-form-radio-group v-model="filters.brand">
-              <!-- Generate radio buttons from an array of brands -->
-              <b-form-radio v-for="brand in brands" :key="brand" :value="brand">{{ brand }}</b-form-radio>
-            </b-form-radio-group>
-          </b-form-group>
-          <!-- More filters can be added similarly -->
-        </b-col>
-
-        <!-- Product Listings -->
-        <b-col cols="9">
-          <b-row>
-            <!-- Product Component -->
-            <b-col lg="2" md="4" sm="6" v-for="product in sortedProducts" :key="product.id">
-              <ProductCard :product="product" />
-            </b-col>
-          </b-row>
-        </b-col>
-      </b-row>
-    </b-container>
-    <Footer></Footer>
-  </div>
+    <!-- Footer Section -->
+    <b-row>
+      <b-col>
+        <b-footer class="mt-3">
+          <!-- Footer content -->
+        </b-footer>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-// Import the ProductCard component
-import ProductCard from '~/components/ProductCard';
-import Header from "~/components/Header";
-import Footer from "../../components/Footer";
-
 export default {
   name: 'User',
-  components: {
-    Footer,
-    Header,
-    ProductCard
-  },
   data() {
     return {
-      sortKey: 'price',
-      sortOptions: [
-        { value: 'price', text: 'Sort by Price' },
-        { value: 'stock', text: 'Sort by Stock' }
-      ],
-      filters: {
-        brand: null
-      },
-      brands: ['Penguin', 'HarperCollins', 'Random House'],
-      products: [
-        { id: 1, name: 'Book Title 1', price: 19.99, stock: 10, imageUrl: 'path-to-image-1.jpg', description: 'Description for product 1' },
-        { id: 2, name: 'Book Title 2', price: 29.99, stock: 5, imageUrl: 'path-to-image-2.jpg', description: 'Description for product 2' },
-        { id: 3, name: 'Book Title 3', price: 9.99, stock: 20, imageUrl: 'path-to-image-3.jpg', description: 'Description for product 3' }
-        // Add more books as needed
+      // Replace with your card data
+      cards: [
+        { title: 'Card Title 1', img: 'image1.jpg', text: 'Some quick example text.' },
+        { title: 'Card Title 2', img: 'image2.jpg', text: 'Some quick example text.' },
+        // ...
       ]
-    };
-  },
-  computed: {
-    sortedProducts() {
-      return [...this.products].sort((a, b) => {
-        if (this.sortKey === 'price') {
-          return a.price - b.price;
-        } else if (this.sortKey === 'stock') {
-          return b.stock - a.stock;
-        }
-      });
-    }
-  },
-  methods: {
-    sortProducts() {
-      // Sorting logic is handled in the computed property
     }
   }
-};
+}
 </script>
 
-<style scoped>
-/* Scoped styles for your component */
-
-.navbar-dark .navbar-brand {
-  color: white;
-}
-
-.navbar-light {
-  background-color: #f8f9fa;
-}
-
-.sidebar-filters {
-  border-right: 1px solid #ddd;
-  padding-right: 15px;
-}
-
-.product-grid {
-  margin-top: 15px;
-}
-
-.product-card {
-  border: 1px solid #ddd;
-  border-radius: 4px;
-  padding: 10px;
-  margin-bottom: 15px;
-  transition: box-shadow .3s ease-in-out;
-}
-
-.product-card:hover {
-  box-shadow: 0 2px 5px rgba(0,0,0,.2);
-}
-
-.mt-3 {
-  margin-top: 1rem;
-}
-
-.ml-auto {
-  margin-left: auto;
-}
+<style>
+/* Here you can add your CSS styles */
 </style>
