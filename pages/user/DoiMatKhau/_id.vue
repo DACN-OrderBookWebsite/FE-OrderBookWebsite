7ae710fca383a6270b0d46ac945d339a3da72183
hiiragiakame <hoangminhlong3006@gmail.com>
2023-12-18 10:58:54 +0700

    Sửa lỗi đổi mật khẩu

--- pages/user/DoiMatKhau/_id.vue
+++ pages/user/DoiMatKhau/_id.vue
@@ -5,7 +5,6 @@
 </template>
     
   <script>
-import Swal from "sweetalert2";
 import QuenMatKhau from "~/components/QuenMatKhau.vue";
 import Header from "~/components/Header";
 import Footer from "~/components/Footer";
@@ -14,16 +13,10 @@ export default {
   components: { Footer, Header, QuenMatKhau },
   data() {
     return {
-      data: {},
-      dataerror: {
-        name: "",
-      },
-      quyen: 1,
     };
   },
   async mounted() {
     await this.checkQuyen();
-    await this.fetch();
   },
   methods: {
     async checkQuyen() {
@@ -31,60 +24,10 @@ export default {
       if (response.length === 0) {
         this.$router.push("/loginkeycloak");
       } else {
-        const kq = await PhanQuyenService.checkQuyen(
-          this.$axios,
-          response[0].id,
-          this.quyen
-        );
-        console.log(kq.data.result);
-        if (kq.data.result === false) {
-          this.$router.push("/");
+        if(this.$route.params.id !== response[0].id)
+        {
+          this.$router.push("/user/DoiMatKhau/" + response[0].id);
         }
-      }
-    },
-    async fetch() {
-      try {
-        const response = await ChucVuService.getItem(
-          this.$axios,
-          this.$route.params.id
-        );
-        this.data = response;
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    confirmUpdate() {
-      Swal.fire({
-        title: "Xác nhận cập nhật",
-        text: "Bạn có chắc chắn muốn cập nhật thông tin?",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Có, cập nhật!",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          console.log(123);
-          this.update();
-        }
-      });
-    },
-    async update() {
-      try {
-        await ChucVuService.update(
-          this.$axios,
-          this.$route.params.id,
-          this.data
-        );
-        Swal.fire(
-          "Cập nhật!",
-          "Thông tin đã được cập nhật thành công.",
-          "success"
-        );
-        this.$router.push("/admin/ChucVu");
-      } catch (error) {
-        this.dataerror = error.response.data.errors;
-        Swal.fire("Lỗi!", "Có lỗi xảy ra khi cập nhật thông tin.", "error");
       }
     },
   },
